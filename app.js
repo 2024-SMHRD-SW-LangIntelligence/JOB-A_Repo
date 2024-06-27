fa802fafb48e53a20b819fba8c0e3baf5d28bbd1
sntari <kdh39554@gmail.com>
2024-06-27 11:40:06 +0900

    app.js 주석설정

--- app.js
+++ app.js
@@ -1,21 +1,24 @@
 const express = require('express');
 const path = require('path');
 
+// express 객체 생성
 const app = express();
 
-// Set the view engine to ejs
+// ejs를 템플릿 엔진으로 사용하도록 설정
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'public')); // Change views directory to public
+// 뷰 템플릿 파일이 위치한 디렉토리를 public으로 설정
+app.set('views', path.join(__dirname, 'public'));
 
-// Static files
+// 정적 파일을 제공하기 위한 미들웨어 설정
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Routes
+// 루트 경로에 접속 시 index.ejs 템플릿을 렌더링하고 title 변수를 전달
 app.get('/', (req, res) => {
   res.render('index', { title: 'My EJS Page' });
 });
 
-const PORT = process.env.PORT || 3379;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+// 3379 포트를 사용
+
+app.listen(3379, () => {
+  console.log(`서버 가동`);
 });
