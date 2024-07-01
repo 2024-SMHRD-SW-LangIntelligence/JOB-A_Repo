0a4f43f952eb9775acc941f3a149a84f234665ec
tritera <tritera9227@gmail.com>
2024-07-01 20:51:17 +0900

    Initial commit

--- app.js
+++ app.js
@@ -95,3 +95,16 @@ newUser.save()
         console.error('사용자 저장 중 오류 발생:', err);
     });
 // ------------------------------MongoDB 관련 설정 끝---------------------------------
+
+// ------------------------------supabase 관련 설정 ----------------------------------
+const { createClient } = require('@supabase/supabase-js');
+
+// Supabase 프로젝트 URL과 API 키
+const supabaseUrl = 'https://gxecvthffkqeuxqkckhr.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd4ZWN2dGhmZmtxZXV4cWtja2hyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTk4MDg4NTEsImV4cCI6MjAzNTM4NDg1MX0.Jh8Hj2qLWHvmiat4jLn6wsnyofwvHUL06lSul367nek';
+
+// Supabase 클라이언트 생성
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+
+// ------------------------------supabase 관련 설정 끝 ------------------------------
